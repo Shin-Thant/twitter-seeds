6f1f64ad26552d577698d280671586921ce838dc
Shin-Thant <shinthant10684@gmail.com>
2023-12-27 17:06:52 +0630

    update config

--- config.ts
+++ config.ts
@@ -1,4 +1,4 @@
-export const DB_URL = process.env.LOCAL_DATABASE_URL as string;
+export const DB_URL = process.env.DATABASE_URL as string;
 export const USERS_COUNT = 35;
 export const TWEETS_COUNT = 35;
 export const SHARES_COUNT = 10;
